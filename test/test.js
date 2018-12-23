b31b82b667c17c63a8cbf1998b35180ea43491a9
Tristan <tristan.morisot@viacesi.fr>
2018-12-23 01:16:21 +0100

    Add test

---
+++ test/test.js
@@ -0,0 +1,14 @@
+/*!
+ * google-drive-manager
+ * Copyright(c) 2018 - 2018 Tristan Morisot <tristan.morisot@viacesi.fr>
+ * MIT Licensed
+ */
+
+const expect = require('chai').expect;
+describe('Google drive manager', () => {
+  describe('InitOAuth2Client', () => {
+    it('should change oauthClient Object', function () {
+      expect(null).to.be.equal(null);
+    });
+  })
+});
